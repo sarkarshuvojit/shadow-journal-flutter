e9650e16bbf66a27670a6f13844490a3298aeb2d
Shuvojit <s15sarkar@yahoo.com>
2020-07-10 09:15:12 +0530

    (wip) Home title added

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -4,11 +4,16 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(children: <Widget>[
-        Column(children: <Widget>[
-          Text('Home')
-        ],)
-      ], mainAxisAlignment: MainAxisAlignment.spaceEvenly,)
+      padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
+      child: Row(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Text('Home', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
